45ebdfa51bd07c60756ef166789c7f870297a554
MatheDoc <sven.fuehring@gmail.de>
2026-04-19 22:15:22 +0200

    a

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mathechecks-cache-v21"; // Version hochsetzen!
+const CACHE_NAME = "mathechecks-cache-v22"; // Version hochsetzen!
 const urlsToCache = [
   "/",
   "/manifest.json",
