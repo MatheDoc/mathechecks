c5dd0b8e8d1ca5019608a31550354126e888436b
Sven Führing <sven.fuehring@gmail.com>
2026-09-25 11:39:57 +0200

    Remove outdated service worker and clear cache on new installation

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+// Kill-Switch: ersetzt den frueheren Caching-Service-Worker und entfernt sich selbst.
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({ type: "window" });
+    clients.forEach((client) => client.navigate(client.url));
+  })());
+});
